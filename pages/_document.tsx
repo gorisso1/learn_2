d09bc4a2a0f70f5a0b2da00f580a92a90d4dd143
Gorisso-manuchi <88540461+Gorisso-manuchi@users.noreply.github.com>
2023-12-27 20:39:36 +0300

    new-6

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export  default function Document() {
     return (
         <Html lang="en">
             <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
                 <link rel="preconnect" href="https://fonts.googleapis.com"/>
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                         <link
